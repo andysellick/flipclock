2ba9211ecb29452fff26b8e034f39b97837fe77c
andysellick <andysellick@builtvisible.com>
2016-04-22 15:08:06 +0100

    Code restructuring

--- js/plugin.js
+++ js/plugin.js
@@ -10,40 +10,40 @@
 	Plugin.prototype = {
 		init: function(){
 			var obj = this;
-			obj.currNum;
             obj.units = ['second','minute','hour','day','month','year'];
-            obj.time = {'year':'00','month':'00','day':'00','hour':'00','minute':'00','second':'00'};
-			obj.prevtime = {'year':'00','month':'00','day':'00','hour':'00','minute':'00','second':'00'};
+            obj.time = {'year':0,'month':0,'day':0,'hour':0,'minute':0,'second':0};
+			obj.prevtime = {'year':0,'month':0,'day':0,'hour':0,'minute':0,'second':0};
 			obj.timelimits = {'year':10000,'month':11,'day':366,'hour':23,'minute':59,'second':59};
 			obj.timer = 0;
 			obj.trigger = 0;
-			obj.countdown = 0; //if true means we are counting down
+			obj.endofdays = 0; //handles the special case when a countdown becomes a countup
 
 			this.settings = $.extend({
 				mode: 'counter', //determines the behaviour of the flipclock: counter simply counts the time since/til targetDate
-				startNum: 0,
-				endNum: 100,
-				speed: 1000,
 				showUnits: true, //controls display name of units beneath digits
 				showAllDigits: false, //if true, show all digits regardless, if false, only show from the 1st digit that is 1 or higher
-				targetDate: [2014,1,1,0,0,0,0],
+				targetDate: [2016,1,1,0,0,0], //fixme number of months seems to be off by 1 - is this the zero-indexed month problem?
+				stopAtZero: false
 			}, this.defaults, this.options);
 
 			var functions = {
                 general: {
 					//do initial settings setup and prevent settings abuse
                     overrideSettings: function(){
+						var len = obj.settings.targetDate.length;
+						if(len < 6){
+							for(var l = len; len < 6; len++){
+								obj.settings.targetDate.push(0);
+							}
+						}
 						var d = obj.settings.targetDate;
 						obj.settings.targetDate = new Date(d[0],d[1] - 1,d[2],d[3],d[4],d[5]); //second parameter, month, is zero indexed, so we have to subtract 1 to get the right month
-						obj.currNum = obj.settings.startNum; //fixme
-						if(obj.settings.targetDate.length < 6){
-
-						}
                     },
                     //insert all required markup
 					initElements: function(){
 						obj.$elem.html('').addClass('timewrap');
-
+						var showdigits = 1;
+						
 						if(obj.settings.mode == 'clock'){
 							//cheat a bit - set the targetDate to an arbitrary midnight, then only show the hours, minutes and seconds
 							obj.settings.targetDate = [2000,1,1,0,0,0,0];
@@ -51,31 +51,25 @@
 							obj.time.second = date.getSeconds();
 							obj.time.minute = date.getMinutes();
 							obj.time.hour = date.getHours();
-	
-							for(var i = obj.units.length - 1; i > -1; i--){
-								if(obj.time[obj.units[i]] !== '00'){
-									functions.markup.generateDigit(functions.general.padDigits(obj.time[obj.units[i]], 2),obj.units[i]);
-								}
-							}
+							showdigits = Math.floor(obj.units.length / 2 - 1);
 						}
 						else {
-							var output = functions.general.getTimeNowTarget();
-							//create all the digits as blank to start with, unless we find any that match from output
-							for(var i = obj.units.length - 1; i >= 0; i--){
-								var ind = output.indexOf(obj.units[i] + 's');
-								if(ind !== -1){
-									functions.markup.generateDigit(functions.general.padDigits(output[ind - 1], 2),obj.units[i]);
-								}
-								else {
-									functions.markup.generateDigit('00',obj.units[i]);
-								}
-							}
+							showdigits = obj.units.length - 1;
 						}
-						//create the timekeeping element
-						obj.trigger = $('<div/>').addClass('trigger').attr('data-id','trigger');
-						obj.trigger.appendTo(obj.$elem);
-						//start the counter
-						functions.general.initCounter();
+						//create digits for all the time units
+						for(var i = showdigits; i > -1; i--){
+							functions.markup.generateDigit(functions.general.padDigits(obj.time[obj.units[i]], 2),obj.units[i]);
+						}
+			            functions.general.backupTime();
+						if(!obj.settings.stopAtZero){
+							//create the timekeeping element
+							obj.trigger = $('<div/>').addClass('trigger').attr('data-id','trigger').appendTo(obj.$elem);
+							//start the counter
+							functions.general.initCounter();
+						}
+						else {
+							functions.general.showDigit('second');
+						}
 
 						if(obj.settings.showAllDigits){
 							obj.$elem.find('.timeunit').addClass('shown'); //show all time elements
@@ -84,16 +78,30 @@
 
 					//get the difference between the time now and the time target
 					getTimeNowTarget: function(){
+			            functions.general.backupTime();
+			            functions.general.resetTime();
 			            var now = Date.now();
-			            if(now < obj.settings.targetDate){
-							obj.countdown = 1;
+			            var output = moment.preciseDiff(now, obj.settings.targetDate);
+			            output = output.split(" ");
+
+		                if(output.length > 1){
+							obj.endofdays = 0;
+				            for(var i = 0; i < output.length; i += 2){
+				                var num = output[i];
+				                var unit = output[i + 1];
+				                if(unit[unit.length - 1] == 's'){ //moment's output can vary e.g. 'seconds' or 'second', to match the array keys in time we need to trim this
+				                    unit = unit.substring(0, unit.length - 1);
+				                }
+				                obj.time[unit] = num; //this only happens if moment returns a value for this digit type, which it doesn't do if it's 0
+				            }
 						}
 						else {
-							obj.countdown = 0;
+							obj.endofdays = 1;
+							if(obj.settings.stopAtZero){
+								obj.trigger.remove();
+								clearTimeout(obj.timer);
+							}
 						}
-			            var output = moment.preciseDiff(now, obj.settings.targetDate);
-			            //console.log(now,obj.settings.targetDate,output);
-			            return(output.split(" "));
 					},
 
 					//set up the counter element and its CSS transition trigger to call the time update
@@ -128,7 +136,6 @@
 						obj.transitionEvent = whichTransitionEvent();
 
 					    //since CSS animation and JS timing of 1 second seems to differ, we trigger the next loop of the JS after a trigger element's transition is complete
-					    //$('.trigger').on(transitionEvent,function(event) {
 						obj.trigger.on(obj.transitionEvent,function(event) {
 					        // Do something when the transition ends
 					        clearTimeout(obj.timer);
@@ -144,76 +151,73 @@
 					    });
 					},
 
+					//called continually to update the display
 					doCounter: function(){
-			            functions.general.resetTime();
 			            var output = functions.general.getTimeNowTarget();
-
-		                if(output.length > 1){
-				            for(var i = 0; i < output.length; i += 2){
-				                var num = output[i];
-				                var unit = output[i + 1];
-				                if(unit[unit.length - 1] == 's'){ //moment's output can vary e.g. 'seconds' or 'second', to match the array keys in time we need to trim this
-				                    unit = unit.substring(0, unit.length - 1);
-				                }
-				                //console.log(unit,obj.time[unit],num);
-				                obj.time[unit] = num; //this only happens if moment returns a value for this digit type, which it doesn't do if it's 0
-				            }
-						}
-						else {
-							console.log('um');
-							//fixme need a condition here for outputting all zeroes - this is the moment a countdown becomes a countup. Maybe a flag to halt as well?
-						}
 			            var ok = 0;
 
-			            for(var key in obj.time){
-			                if(obj.time[key] != '00' || ok){ //fixme this is supposed to only flag once then allow continuation through the digits, but it doesn't work
-			                    ok = 1;
-			                    var unit = key;
-			                    var digit = parseInt(obj.time[key]);
-			                    if(digit != 1){
-			                        unit = unit + 's'; //add the s back on
-			                    }
-
-			                    var prevdigit = digit - 1; //get the next digit, for the animation
-			                    if(obj.countdown){
-									prevdigit = digit + 1;
-				                    if(prevdigit > obj.timelimits[key]){
-										prevdigit = 0;
-									}
+			            for(var u = obj.units.length - 1; u >= 0 ; u--){
+							functions.general.updateDigit(obj.units[u]);
+							//show or hide the digit if it is zero and it was last time, unless it's seconds, in which case we always show it
+							if(ok){ //fixme should be better way of structuring this
+								functions.general.showDigit(obj.units[u]);
+							}
+							else {
+								if(obj.time[obj.units[u]] === 0 && obj.prevtime[obj.units[u]] === 0 && obj.units[u] !== 'second'){
+									functions.general.hideDigit(obj.units[u]);
 								}
 								else {
-				                    if(prevdigit < 0){
-				                        prevdigit = obj.timelimits[key];
-				                    }
+									ok = 1;
+									functions.general.showDigit(obj.units[u]);
 								}
-			                    if(digit < 0){
-			                        digit = obj.timelimits[key];
-			                    }
-			                    digit = functions.general.padDigits(digit, 2)
-			                    prevdigit = functions.general.padDigits(prevdigit, 2)
-
-			                    var wrapper = obj.$elem.find('.timeunit[data-name=' + key + ']').attr('class','timeunit shown');//find the relevant element and set it up
-			                    var digits = wrapper.find('.digits').attr('data-next',digit).attr('data-now',prevdigit);
-
-			                    digits.find('.next').remove();
-			                    digits.find('.prev').remove();
-
-			                    $('<div/>').addClass('next').html(digit).appendTo(digits);
-			                    $('<div/>').addClass('prev').html(prevdigit).appendTo(digits);
-			                    digits.find('.number').html(digit);
-			                    if(obj.settings.showUnits){
-				                    wrapper.find('.units').html(unit);
-								}
-
-			                    if(obj.time[key] != obj.prevtime[key] || (digit == '00' && key == 'seconds')){ //only animate if changed or unit is 0 and seconds. Otherwise at 0 just animates forever
-			                        digits.attr('class','digits animate'); //in theory this is quicker than addClass
-			                    }
-			                    else {
-			                        digits.attr('class','digits');
-			                    }
-			                }
-			            }
+							}
+						}
 			            functions.general.backupTime();
+					},
+					
+					//update the contents of a single time unit
+					updateDigit: function(key){
+	                    var unit = key;
+	                    var digit = parseInt(obj.time[key]);
+	                    if(digit != 1){
+	                        unit = unit + 's'; //add the s back on
+	                    }
+	                    var prevdigit = obj.prevtime[key];
+	                    digit = functions.general.padDigits(digit, 2)
+	                    prevdigit = functions.general.padDigits(prevdigit, 2)
+
+	                    var wrapper = obj.$elem.find('.timeunit[data-name=' + key + ']'); //.attr('class','timeunit shown');//find the relevant element and set it up
+	                    var digits = wrapper.find('.digits').attr('data-next',digit).attr('data-now',prevdigit);
+
+	                    digits.find('.next').remove();
+	                    digits.find('.prev').remove();
+	                    $('<div/>').addClass('next').html(digit).appendTo(digits);
+	                    $('<div/>').addClass('prev').html(prevdigit).appendTo(digits);
+
+	                    digits.find('.number').html(digit);
+
+	                    if(obj.settings.showUnits){
+		                    wrapper.find('.units').html(unit);
+						}
+
+	                    if(obj.time[key] !== obj.prevtime[key]){ //only animate if changed
+	                        digits.attr('class','digits animate'); //in theory this is quicker than addClass
+	                    }
+	                    else {
+	                        digits.attr('class','digits');
+	                    }					
+					},
+					
+					//show a particular time unit
+					showDigit: function(key){
+						obj.$elem.find('.timeunit[data-name=' + key + ']').addClass('shown');
+					},
+
+					//hide a particular time unit
+					hideDigit: function(key){
+						if(!obj.settings.showAllDigits){
+							obj.$elem.find('.timeunit[data-name=' + key + ']').removeClass('shown');
+						}
 					},
 					
 			        //store time for comparison next time round, since it's an object can't do a direct copy
@@ -222,9 +226,9 @@
 			                obj.prevtime[y] = obj.time[y];
 			            }
 			        },
-			        //set the time back to zero
+			        //set the time back to zero, needed to handle when we reach zero for numbers
 			        resetTime: function(){
-			            obj.time = {'year':'00','month':'00','day':'00','hour':'00','minute':'00','second':'00'};
+			            obj.time = {'year':0,'month':0,'day':0,'hour':0,'minute':0,'second':0};
 			        },
 
 					//get the number of digits in a number
